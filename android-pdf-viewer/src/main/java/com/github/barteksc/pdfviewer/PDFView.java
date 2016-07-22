3cff36652bb7f7972d24de730feb3b6ec7cec37f
Miha-x64 <silizium@ok.de>
2016-07-22 17:32:09 +0400

    Draw the specified background if present

--- android-pdf-viewer/src/main/java/com/github/barteksc/pdfviewer/PDFView.java
+++ android-pdf-viewer/src/main/java/com/github/barteksc/pdfviewer/PDFView.java
@@ -494,7 +494,12 @@ public class PDFView extends SurfaceView {
         // abstraction of the screen position when rendering the parts.
 
         // Draws background
-        canvas.drawColor(Color.WHITE);
+        Drawable bg = getBackground();
+        if (bg == null) {
+            canvas.drawColor(Color.WHITE);
+        } else {
+            bg.draw(canvas);
+        }
 
         if(recycled) {
             return;
