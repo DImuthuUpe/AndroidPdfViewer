5845b4134901107a3fbf17c049003ada8e6da6f8
kenneth.nickles <kenneth.nickles@workday.com>
2021-05-12 14:46:40 -0700

    jetpack migration

--- sample/src/main/java/com/github/barteksc/sample/PDFViewActivity.java
+++ sample/src/main/java/com/github/barteksc/sample/PDFViewActivity.java
@@ -22,10 +22,10 @@ import android.database.Cursor;
 import android.graphics.Color;
 import android.net.Uri;
 import android.provider.OpenableColumns;
-import android.support.annotation.NonNull;
-import android.support.v4.app.ActivityCompat;
-import android.support.v4.content.ContextCompat;
-import android.support.v7.app.AppCompatActivity;
+import androidx.annotation.NonNull;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+import androidx.appcompat.app.AppCompatActivity;
 import android.util.Log;
 import android.widget.Toast;
 
